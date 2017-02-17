29b99a68cc23e241baa51b908a5d0555b3a0a82f
filiperfernandes <filipe.ramalhofernandes@gmail.com>
2017-02-17 19:19:52 +0000

    Added users integration with accounts

--- trazme/collections/users.js
+++ trazme/collections/users.js
@@ -1,44 +1,61 @@
-// Users = new Meteor.Collection('users');
+// import { Meteor } from 'meteor/meteor';
+// import { AccountsTemplates } from 'meteor/accounts-base';
 
-// UserSchema = new SimpleSchema({
 
-// 	username: {
-// 		type: String,
-// 		label: "Username"
-// 	},
-
-// 	name: {
-// 		type: String,
-// 		label: "name"
-// 	},
-
-// 	mail: {
-// 		type: String,
-// 		label: "mail"
-// 	},
-
-// 	pcode: {
-// 		type: Number,
-// 		label: "pcode"
-// 	},
-
-// 	nif: {
-// 		type: Number,
-// 		label: "nif"
-// 	},
-
-// 	phone: {
-// 		type: Number,
-// 		label: "phone"
-// 	},
-
-// 	createdAt: {
-// 		type: Date,
-// 		label: "Created At",
-// 		autoValue: function() {
-// 			return new Date()
-// 		}
-// 	}
-// });
-
-// Users.attachSchema(UserSchema);
+var pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+      _id: "username",
+      type: "text",
+      displayName: "username",
+      required: true,
+      minLength: 5,
+  },
+  {
+      _id: 'email',
+      type: 'email',
+      required: true,
+      displayName: "email",
+      re: /.+@(.+){2,}\.(.+){2,}/,
+      errStr: 'Invalid email',
+  },
+  {
+      _id: 'username_and_email',
+      type: 'text',
+      required: true,
+      displayName: "Login",
+  },
+  pwd,
+  {
+    _id: 'name',
+    type: 'text',
+    required: true,
+    displayName: "Name",
+  },
+  {
+  	_id: 'phone',
+    type: 'tel',
+    displayName: "Phone",
+    required: true,
+  },
+  {
+    _id: 'NIF',
+    type: 'text',
+    displayName: 'NIF',
+    required: true,
+  },
+  {
+    _id: 'pcode',
+    type: 'text',
+    displayName: 'Postal Code',
+    required: true,
+  },
+  {
+    _id: 'date',
+    type: 'hidden',
+    autoValue: function() {
+      return new Date()
+    }
+  },
+]);
