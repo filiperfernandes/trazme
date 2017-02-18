736d858a111122d4423e5915c825c15c39a0a2c6
TJacoob <tomas.j.martins@ist.utl.pt>
2017-02-18 13:25:02 +0000

    Item Add and All implemented

---
+++ trazme/client/items/itemAll.js
@@ -0,0 +1,62 @@
+Template.itemAll.onCreated(function() {
+	var self = this;
+
+	this.selOrigin = new ReactiveVar();
+	this.selDestination = new ReactiveVar();
+
+	self.autorun(function(){
+		self.subscribe('item');
+		Session.set("selOrigin","");
+		Session.set("selDestination","");
+	});
+});
+
+Template.itemAll.helpers({
+	allItems: function(){
+		var filter = {};
+		var sO = Session.get("selOrigin");
+		var sD = Session.get("selDestination");
+		if ( sO )
+			filter.origin = sO ;
+		if ( sD )
+			filter.destination = sD ;
+		return Item.find(filter);
+	},
+	allOrigins: function(){
+		return distinct( Item,"origin" ) ;
+	},
+	allDestinations: function(){
+		return distinct( Item, "destination") ;
+	},
+	selOr: function(){
+		return Session.get("selOrigin");
+	},
+	selDest: function(){
+		return Session.get("selDestination");
+	},
+
+});
+
+Template.itemAll.events({
+	"click .selOrigin": function(e, t){
+		var text = $(e.target).text();
+		Session.set("selOrigin",text);
+	},
+	"click .orReset": function(e, t){
+		Session.set("selOrigin","");
+	},
+	"click .selDestination": function(e, t){
+		var text = $(e.target).text();
+		Session.set("selDestination",text);
+	},
+	"click .destReset": function(e, t){
+		Session.set("selDestination","");
+	},
+});
+
+
+function distinct(collection, field) {
+  return _.uniq(collection.find({}, {
+    sort: {[field]: 1}, fields: {[field]: 1}
+  }).map(x => x[field]), true);
+}
