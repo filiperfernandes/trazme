79bcef140642b43fbf06cde0fa58bf5cee149f74
filiperfernandes <filipe.ramalhofernandes@gmail.com>
2017-02-18 14:12:51 +0000

    add insert function

--- trazme/server/main.js
+++ trazme/server/main.js
@@ -3,3 +3,22 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+Meteor.methods({
+
+	'transactions.insert'(type, orderId, userA, userB, state){
+
+		var info = ""; 
+
+		Transactions.insert({
+	      type: type,
+	      orderId: orderId,
+	      userA: userA,
+	      userB: userB,
+	      state: state,
+	      info: info,
+	      createdAt: new Date() // current time
+	    });
+	}
+});
