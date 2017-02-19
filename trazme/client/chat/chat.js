e834c7e848a60a6d66fbce0e595a526efdf67ca8
filiperfernandes <filipe.ramalhofernandes@gmail.com>
2017-02-19 14:06:49 +0000

    Map feature is now dynamic
    Google API Key needed for API requests

--- trazme/client/chat/chat.js
+++ trazme/client/chat/chat.js
@@ -1,3 +1,8 @@
+Template.chat.onRendered(function() {
+  GoogleMaps.load({key:"INSER YOUR GOOGLE API"});
+});
+
+
 Template.chat.onCreated(function() {
 	var self = this;
 
@@ -9,7 +14,101 @@ Template.chat.onCreated(function() {
 		self.subscribe('item');
 		self.subscribe('allUsers');
 	});
+
+
+	// We can use the `ready` callback to interact with the map API once the map is ready.
+
+
+  GoogleMaps.ready('lxMap', function(map) {
+    // Add a marker to the map once it's ready
+    // var marker = new google.maps.Marker({
+    //   position: map.options.center,
+    //   map: map.instance
+    // });
+
+    var id = FlowRouter.getParam('id');
+    let oId = Transactions.findOne({_id:id}).orderId ;
+    var torigin = Trip.findOne({_id:oId}).origin;
+    var tdestination = Trip.findOne({_id:oId}).destination;
+
+    var origin;
+    var destination;
+
+   	switch (torigin) {
+	    case "Lisboa":
+	    	origin=  {lat: 38.73, lng: -9.14};
+	    	break;
+	        
+	    case "Coimbra":
+	    	origin=  {lat: 40.22, lng: -8.59};
+	     break;
+
+	     case "Santarém":
+	    	origin=  {lat: 39.32, lng: -8.86};
+	     break;
+
+	     case "Porto":
+	    	origin=  {lat: 41.16, lng: -8.69};
+	     break;
+
+	     case "Faro":
+	    	origin=  {lat: 37.01, lng: -7.97};
+	     break;
+   	}
+
+   	switch (tdestination) {
+	    case "Lisboa":
+	    	destination=  {lat: 38.73, lng: -9.14};
+	        break;
+	    case "Coimbra":
+	    	destination =  {lat: 40.22, lng: -8.59};
+	     break;
+	   	case "Santarém":
+	    	destination=  {lat: 39.32, lng: -8.86};
+	     break;
+
+	     case "Porto":
+	    	destination=  {lat: 41.16, lng: -8.69};
+	     break;
+
+	     case "Faro":
+	    	destination=  {lat: 37.01, lng: -7.97};
+	     break;
+   	}
+
+
+    // var origin = {lat: 38.73, lng: -9.14};
+    // var destination = {lat: 40.22, lng: -8.59};
+
+    
+
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //       center: chicago,
+    //       scrollwheel: false,
+    //       zoom: 7
+    // });
+
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+          map: map.instance
+    });
+
+    // Set destination, origin and travel mode.
+    var request = {
+        destination: destination,
+        origin: origin,
+        travelMode: 'DRIVING'
+    };
+
+    // Pass the directions request to the directions service.
+    var directionsService = new google.maps.DirectionsService();
+    directionsService.route(request, function(response, status) {
+        if (status == 'OK') {
+        	// Display the route on the map.
+            directionsDisplay.setDirections(response);
+        }
+    });
 });
+  });
 
 
 Template.chat.helpers({
@@ -60,6 +159,17 @@ Template.chat.helpers({
 		let t = Trip.findOne({_id: oId }).user;	
 		return Mongo.Collection.get('users').findOne({_id:t}).username ;
 	},
+
+	exampleMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(38.73, -9.14),
+        zoom: 8
+      };
+    }
+  }
 
 
 });
--- trazme/client/map.js
+++ trazme/client/map.js
@@ -1,66 +1,112 @@
-Template.gMap.onRendered(function() {
-  GoogleMaps.load({key:""});
-});
+// Template.chat.onRendered(function() {
+//   GoogleMaps.load({key:"AIzaSyDYGVe-E4UOD1W9xEOvNHW44ng0xUC5j3Y"});
+// });
 
 
-Template.gMap.helpers({
-  exampleMapOptions: function() {
-    // Make sure the maps API has loaded
-    if (GoogleMaps.loaded()) {
-      // Map initialization options
-      return {
-        center: new google.maps.LatLng(38.73, -9.14),
-        zoom: 8
-      };
-    }
-  }
-});
+// Template.chat.helpers({
+//   exampleMapOptions: function() {
+//     // Make sure the maps API has loaded
+//     if (GoogleMaps.loaded()) {
+//       // Map initialization options
+//       return {
+//         center: new google.maps.LatLng(38.73, -9.14),
+//         zoom: 8
+//       };
+//     }
+//   }
+// });
 
 
 
-Template.gMap.onCreated(function() {
-  // We can use the `ready` callback to interact with the map API once the map is ready.
+// Template.chat.onCreated(function() {
+//   // We can use the `ready` callback to interact with the map API once the map is ready.
 
 
-  GoogleMaps.ready('exampleMap', function(map) {
-    // Add a marker to the map once it's ready
-    // var marker = new google.maps.Marker({
-    //   position: map.options.center,
-    //   map: map.instance
-    // });
+//   GoogleMaps.ready('lxMap', function(map) {
+//     // Add a marker to the map once it's ready
+//     // var marker = new google.maps.Marker({
+//     //   position: map.options.center,
+//     //   map: map.instance
+//     // });
 
-    var origin = {lat: 38.73, lng: -9.14};
-    var destination = {lat: 40.22, lng: -8.59};
+//     var origin = {lat: 38.73, lng: -9.14};
+//     var destination = {lat: 40.22, lng: -8.59};
 
-    // var map = new google.maps.Map(document.getElementById('map'), {
-    //       center: chicago,
-    //       scrollwheel: false,
-    //       zoom: 7
-    // });
+    
 
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-          map: map.instance
-    });
+//     // var map = new google.maps.Map(document.getElementById('map'), {
+//     //       center: chicago,
+//     //       scrollwheel: false,
+//     //       zoom: 7
+//     // });
 
-    // Set destination, origin and travel mode.
-    var request = {
-        destination: destination,
-        origin: origin,
-        travelMode: 'DRIVING'
-    };
+//     var directionsDisplay = new google.maps.DirectionsRenderer({
+//           map: map.instance
+//     });
 
-    // Pass the directions request to the directions service.
-    var directionsService = new google.maps.DirectionsService();
-    directionsService.route(request, function(response, status) {
-        if (status == 'OK') {
-        	// Display the route on the map.
-            directionsDisplay.setDirections(response);
-        }
-    });
+//     // Set destination, origin and travel mode.
+//     var request = {
+//         destination: destination,
+//         origin: origin,
+//         travelMode: 'DRIVING'
+//     };
+
+//     // Pass the directions request to the directions service.
+//     var directionsService = new google.maps.DirectionsService();
+//     directionsService.route(request, function(response, status) {
+//         if (status == 'OK') {
+//         	// Display the route on the map.
+//             directionsDisplay.setDirections(response);
+//         }
+//     });
 
 
-  });
-});
+//   });
+
+
+//   GoogleMaps.ready('cbMap', function(map) {
+//     // Add a marker to the map once it's ready
+//     // var marker = new google.maps.Marker({
+//     //   position: map.options.center,
+//     //   map: map.instance
+//     // });
+
+//     var origin = {lat: 38.73, lng: -9.14};
+//     var destination = {lat: 40.22, lng: -8.59};
+
+    
+
+//     // var map = new google.maps.Map(document.getElementById('map'), {
+//     //       center: chicago,
+//     //       scrollwheel: false,
+//     //       zoom: 7
+//     // });
+
+//     var directionsDisplay = new google.maps.DirectionsRenderer({
+//           map: map.instance
+//     });
+
+//     // Set destination, origin and travel mode.
+//     var request = {
+//         destination: destination,
+//         origin: origin,
+//         travelMode: 'DRIVING'
+//     };
+
+//     // Pass the directions request to the directions service.
+//     var directionsService = new google.maps.DirectionsService();
+//     directionsService.route(request, function(response, status) {
+//         if (status == 'OK') {
+//           // Display the route on the map.
+//             directionsDisplay.setDirections(response);
+//         }
+//     });
+
+
+//   });
+
+
+// });
 
 
 
